87668fde7d1b8c85eb8110c52eefd815cccd8ad6
tubowen <tubowen@kuaishou.com>
2022-07-12 23:44:24 +0800

    Branch process remove .umi

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'D:/Opensource/RecycleUI/node_modules/umi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
