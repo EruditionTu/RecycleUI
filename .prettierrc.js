54e3c7fe5be1f71c0f90e62692c493f374b0f06e
tubowen <tubowen@kuaishou.com>
2022-10-15 01:20:30 +0800

    feat(root): 为项目添加lint，commitizen

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,15 @@
-module.exports={
-  semi: false,
-  tabWidth: 2,
-  trailingComma: "none",
-  singleQuote: true,
-  arrowParens: "avoid"
-}
+module.exports = {
+  singleQuote: true, // 字符串使用单引号
+  trailingComma: 'all', // 尽可能的使用尾随逗号
+  printWidth: 100, // 打印宽度
+  proseWrap: 'never', // 强制所有散文块位于一行
+  arrowParens: 'avoid', // 省略箭头函数参数的括号
+  overrides: [
+    {
+      files: '.babelrc', // 识别特殊配置文件
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
