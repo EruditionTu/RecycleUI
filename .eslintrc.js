544e573234c7645ff2e1db96866180e07ad8c6f8
tubowen <tubowen@kuaishou.com>
2022-10-15 16:16:41 +0800

    feat(root): eslint添加extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:compat/recommended',
+    'standard-with-typescript',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    polyfills: ['Promise', 'URL'],
+  },
+  parser: '@typescript-eslint/parser', // eslint默认espree作为parser
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   singleQuote: true, // 字符串使用单引号
   trailingComma: 'all', // 尽可能的使用尾随逗号
-  printWidth: 100, // 打印宽度
+  printWidth: 80, // 打印宽度
   proseWrap: 'never', // 强制所有散文块位于一行
   arrowParens: 'avoid', // 省略箭头函数参数的括号
   overrides: [
