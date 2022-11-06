232bad4dd7214c0e6ad7f25900c51731e1224035
tubowen <tubowen@kuaishou.com>
2022-11-06 09:48:00 +0800

    feat(root): refactoring Project Engineering

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  singleQuote: true, // 字符串使用单引号
-  trailingComma: 'all', // 尽可能的使用尾随逗号
-  printWidth: 80, // 打印宽度
-  proseWrap: 'never', // 强制所有散文块位于一行
-  arrowParens: 'avoid', // 省略箭头函数参数的括号
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  proseWrap: 'never',
+  endOfLine: 'lf',
   overrides: [
     {
-      files: '.babelrc', // 识别特殊配置文件
+      files: ['.prettierrc', '.babelrc'],
       options: {
         parser: 'json',
       },
