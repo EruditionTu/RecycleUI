abbc83686f93575f27172657efbce0d198c2faee
EruditionTu <1376490336@qq.com>
2022-11-29 12:55:04 +0800

    feat(common): 修改Portal

--- packages/common/util/toolComponents/Portal.tsx
+++ packages/common/util/toolComponents/Portal.tsx
@@ -1,6 +1,38 @@
-// import React, { forwardRef } from 'react';
-// import ReactDOM from 'react-dom';
+import { forwardRef, useRef, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import type { ReactNode } from 'react';
 
-interface PortalProps {}
+type PortalRef = {};
 
-export { PortalProps };
+interface PortalProps {
+  children?: ReactNode;
+  getContainer: () => HTMLElement;
+}
+
+const Portal = forwardRef<PortalRef, PortalProps>((props, ref) => {
+  const { children, getContainer } = props;
+
+  const parentRef = useRef<ParentNode | null>(null);
+  const containerRef = useRef<HTMLElement | null>(null);
+
+  const initRef = useRef(false);
+
+  if (!initRef.current) {
+    containerRef.current = getContainer();
+    parentRef.current = containerRef.current.parentNode;
+    initRef.current = true;
+  }
+
+  useEffect(() => {
+    if (containerRef.current?.parentNode === null && parentRef.current !== null) {
+      parentRef.current.appendChild(containerRef.current);
+    }
+    return () => {
+      containerRef.current?.parentNode?.removeChild(containerRef.current);
+    };
+  }, []);
+  return containerRef.current ? createPortal(children, containerRef.current) : null;
+});
+
+export default Portal;
+export { PortalProps, PortalRef };
